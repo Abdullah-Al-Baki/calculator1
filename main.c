733219712f14f0f39cec23b6cc8183ad06034caf
Abdullah Al Baki <abdullahalbaki.shuvo@gmail.com>
2019-10-18 01:02:52 +0600

    Sub function added

--- main.c
+++ main.c
@@ -5,7 +5,9 @@ int main()
 {
     int a=100, b=10, sum, sub;
     sum=a+b;
+    sub=a-b;
 
     printf("Sum is = %d \n", sum);
+    printf("Sub of first-second number is = %d \n", sub);
     return 0;
 }
