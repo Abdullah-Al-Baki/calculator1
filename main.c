69a1f4a98a79d6c683a324dbd0935a35c5dbbedd
Abdullah Al Baki <abdullahalbaki.shuvo@gmail.com>
2019-10-18 01:14:35 +0600

    Adding multiplies

--- main.c
+++ main.c
@@ -3,11 +3,13 @@
 
 int main()
 {
-    int a=100, b=10, sum, sub;
+    int a=100, b=10, sum, sub, mul;
     sum=a+b;
     sub=a-b;
+    mul=a*b;
 
     printf("Sum is = %d \n", sum);
     printf("Sub of first-second number is = %d \n", sub);
+    printf("Multiplies is = %d \n", mul);
     return 0;
 }
