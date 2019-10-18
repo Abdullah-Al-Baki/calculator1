76bee673990fe79a6c720b2ceb180fd1682ba91c
Abdullah Al Baki <abdullahalbaki.shuvo@gmail.com>
2019-10-18 10:58:33 +0600

    Division added

--- main.c
+++ main.c
@@ -3,13 +3,15 @@
 
 int main()
 {
-    int a=100, b=10, sum, sub, mul;
+    int a=100, b=10, sum, sub, mul,div;
     sum=a+b;
     sub=a-b;
     mul=a*b;
+    div=a/b;
 
     printf("Sum is = %d \n", sum);
     printf("Sub of first-second number is = %d \n", sub);
     printf("Multiplies is = %d \n", mul);
+    printf("a/b is = %d \n", div);
     return 0;
 }
